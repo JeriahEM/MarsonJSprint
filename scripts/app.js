8b47535acad4580ad5d80fff421a97190a3cbe7a
Jeriah Marson <jeriahmarson@gmail.com>
2023-12-05 16:06:11 -0800

    zip and dip

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-fetch("72cec1161d6381d458f5adde5ea24073")
+
+let day = document.getElementById("day");
+
+
+//fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=72cec1161d6381d458f5adde5ea24073")
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
+    .then((response)=>{
+        //using JSON method to parse into json data
+        return response.json();
+    })
+  
+    .then((data => day.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
