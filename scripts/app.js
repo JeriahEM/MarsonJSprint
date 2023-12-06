8e8b96a377070aeac04a19e7baa699687d59f70a
Jeriah Marson <jeriahmarson@gmail.com>
2023-12-06 15:53:52 -0800

    good work lowkey

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,84 @@
 
-let day = document.getElementById("day");
+let currentDay = document.getElementById("currentDay");
+let descript = document.getElementById("descript");
+let icon = document.getElementById("icon");
+let tempMin = document.getElementById("tempMin");
+//let tempMax = documnet.GetElementById("tempMax");
 
 
-//fetch("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=72cec1161d6381d458f5adde5ea24073")
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
     .then((response)=>{
-        //using JSON method to parse into json data
+
         return response.json();
     })
   
-    .then((data => day.innerHTML =  Math.floor(data.main.feels_like)+ "°"))
+    .then((data => currentDay.innerHTML =  Math.floor(data.main.feels_like)+ "°F"))
+
+
+
+
+
+
+
+
+    navigator.geolocation.getCurrentPosition(success, errorFunc)
+    //You can think of this as an if statement if user runs success else we run errorFunc
+    
+    //Example of the geolocation object below
+    {
+        coords: {
+            latitude: 32.1234;
+            longitude: 13.1234;
+        }
+    }
+    
+    //If user accepts we run success function
+    function success(position){
+        console.log("Our Latitude: " + position.coords.latitude);
+        console.log("our Longitude: " + position.coords.longitude);
+    
+    }
+    //If user denies we run error function
+    function errorFunc(error){
+    console.log(error.message);
+    }
+    
+
+
+
+
+
+
+
+
+
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
+    .then((response)=>{
+
+        return response.json();
+    })
+  
+    .then((data => descript.innerHTML =  Math.floor(data.weather[2].description)))
+
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
+    .then((response)=>{
+
+        return response.json();
+    })
+  
+    .then((data => tempMin.innerHTML =  "↓"+ Math.floor(data.main.temp_min)+ "°F"))
+
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
+    .then((response)=>{
+
+        return response.json();
+    })
+  
+    .then((data => tempMax.innerHTML =  "↑" + Math.floor(data.main.temp_max)+ "°F"))
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Stockton&appid=72cec1161d6381d458f5adde5ea24073&units=imperial`)
+    .then((response)=>{
+        return response.json();
+    })
+
+    .then((data => icon.innerHTML = Math.floor(data.weather[3].icon)))
